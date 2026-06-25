2dbc37b4cc0a5b2abbc6cde202a4d2f21381cba0
RoastedSalsa <augustaspug@gmail.com>
2026-06-25 16:31:37 +0200

    Updated Inverse kinematics and added forward kinematics

--- Mark1/firmware/lib/ArmKinematics/ArmKinematics.cpp
+++ Mark1/firmware/lib/ArmKinematics/ArmKinematics.cpp
@@ -1,13 +1,17 @@
 #include "ArmKinematics.h"
 #include "config.h"   // L1, L2, L3
 
-// Math copied verbatim from full.cpp (only l1/l2/l3 -> L1/L2/L3, and the local
-// theta names -> the t1/t2/t3 out-params). Same float types, same operator
-// precedence, same signs -> identical results.
 bool ik(float x, float y, float z, float& t1, float& t2, float& t3) {
   float r = sqrt(pow(x,2) + pow(y,2));
-  t1 =  atan2(x,y) - PI/2;
-  t3 =  atan2(-sqrt(1-pow(((pow(r,2)+pow((-z+L1),2) -pow(L2,2) - pow(L3,2))/ (2 * L2 * L3)),2) ), (pow(r,2) + pow((-z+L1),2) -pow(L2,2) -pow(L3,2))/(2 * L2 * L3));
-  t2 =  -( atan2(-z + L1, r ) + atan2(L3*sin(t3), L2 + L3*cos(t3)));
+  t1 =  atan2(y,x);
+  t3 =  atan2(-sqrt(1-pow(((pow(r,2)+pow((z-L1),2) -pow(L2,2) - pow(L3,2))/ (2 * L2 * L3)),2) ), (pow(r,2) + pow((z-L1),2) -pow(L2,2) -pow(L3,2))/(2 * L2 * L3));
+  t2 =  ( atan2(z - L1, r ) - atan2(L3*sin(t3), L2 + L3*cos(t3)));
+  return true;
+}
+
+bool fk(float& x, float& y, float& z, float t1, float t2, float t3) {
+  x = cos(t1)*(l2*cos(t2)+l3*cos(t2+t3));
+  y = sin(t1)*(l2*cos(t2)+l3*cos(t2+t3));
+  z = l1 + (l2*sin(t2)+l3*sin(t2+t3));
   return true;
 }
